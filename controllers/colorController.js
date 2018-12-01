5b650ec77a8173781ecdedc82a2f48097cc70ab7
ParcialWeb <00004017@uca.edu.sv>
2018-12-01 16:21:23 -0600

    se termina el main.js

--- controllers/colorController.js
+++ controllers/colorController.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 colorModel = require('../models/color');
 
-colorController = {};
+const colorController = {};
 
 colorController.create = function (req, res) {
     let data = {
@@ -9,14 +9,14 @@ colorController.create = function (req, res) {
         hexa: req.body.hexa,
     };
 
-    if (data.nombre && data.hexa) {
+    if (data.nombre && data.hexa ) {
         let colorNuevo = new colorModel(data);
         colorNuevo.save(function (err, guardado) {
             if (err) {
                 res.status(500);
                 res.json({ code: 500, err });
             } else {
-                res.json({ ok: true });
+                res.json({ ok: true ,message:"guardado con exito"});
             }
         });
     }else {
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,33 +1,71 @@
-window.onload =()=>{
+window.onload = () => {
     app.int;
 }
 
-let app ={
+let app = {
     init: function () {
         this.addEvents;
         this.loadContent;
     },
-    addEvents:function () {
-        document.addEventListener("Submit",(event)=>{
-            this.SubmitPost(event,this.addRow);
+    addEvents: function () {
+        document.addEventListener("Submit", (event) => {
+            this.SubmitPost(event, this.addRow);
         });
     },
     addRow: function (data) {
-        tbody=document.getElementsByClassName("Color")[0];
-        tr=document.createElement('tr');
-        tr.innerHTML=`<td`
+        tbody = document.getElementsByClassName("colors")[0];
+        tr = document.createElement('tr');
+        tr.innerHTML = `<td>${data._id}</td>
+        <td>${data.name}</td>
+        <td>${data.hexa}</td>
+        <td><a href="#" class="delete">Delete</a>
+        <a href="#" class="update">update</a> </td>
+        `;
+
+        tr.getElementsByClassName("delete")[0].addEventListener("click", (event) => {
+            this.deletePost(event, data, tr, tbody);
+        })
     },
 
     deletePost: function () {
-        
+
     },
-    SubmitPost: (event,addRow)=>{
+    SubmitPost: (event, addRow) => {
         event.preventDefault();
-        let data ={
-            
-        }
-    } 
-        
-
-
+        let data = {
+            nombre: document.postForm.nombre.value,
+            hexa: document.postForm.nombre.value
+        };
+        fetch('/api/color', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'aplication.json'
+            }.then((res) => res.json())
+                .then((_data) => {
+                    if (_data.ok) {
+                        addRow(_data.guardado);
+                    } else {
+                        console.log("no se guardo")
+                    }
+                })
+        })
+    },
+    loadContent: function () {
+        fetch('/api/color', {
+            method: 'GET'
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.ok) {
+                    data.color.forEach(element => {
+                        this.addRow(element);
+                    })
+                }
+            })
+    }
 }
+
+
+
+
