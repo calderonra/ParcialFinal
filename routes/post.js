cb08c6c7cff7c04bc2dae83b710bb69f3feebd6f
ParcialWeb <00004017@uca.edu.sv>
2018-12-01 14:14:21 -0600

    Rutas de posteo

--- routes/post.js
+++ routes/post.js
@@ -3,4 +3,11 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/',postController.Create);
+router.post('/',postController.Create);
+router.get('/',postController.get);
+
+router.get('/:id',postController.getId);
+
+router.delete('/:id',postController.delete);
+
+module.exports=router;
